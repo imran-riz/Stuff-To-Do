2f0e2614bc7d9c2277f3bc143d34357959fbbc6e
Imran R <67403229+imran-2003@users.noreply.github.com>
2022-08-12 17:03:21 +0530

    Delete src directory

--- src/values/HomePageHeaders.java
+++
@@ -1,9 +0,0 @@
-package values;
-
-public final class HomePageHeaders {
-    public static final String TASKS_TODAY = "Tasks For Today" ;
-    public static final String PLANNED_TASKS = "Planned" ;
-    public static final String COMPLETED_TASKS = "Tasks Completed" ;
-
-    private HomePageHeaders() {}
-}
